52bd0930578309e9fc3caa7f3278dbb3c5bf951f
chrichri <christophe.brun@papit.fr>
2024-03-23 14:31:45 +0100

    Compile twice for TOC.

--- compile-latex.sh
+++ compile-latex.sh
@@ -4,5 +4,7 @@ if ! [ -x "$(command -v lualatex)" ]; then
   echo 'Error: lualatex is not installed.' >&2
   exit 1
 fi
-# Compile LaTeX with lualatex
-cd doc && lualatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=build/pdf Qualité-Code-Source-L3-CSI.tex
+# Compile LaTeX with lualatex, compile twice to get the table of contents
+cd doc
+lualatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=build/pdf Qualité-Code-Source-L3-CSI.tex
+lualatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=build/pdf Qualité-Code-Source-L3-CSI.tex
